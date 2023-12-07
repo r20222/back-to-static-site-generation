dc94e9b7dd6230cab3efbb98e38eaf1517c5c776
r20222 <101579892+r20222@users.noreply.github.com>
2023-12-07 14:08:11 +0100

    static site generation #11
    github pages

--- svelte.config.js
+++ svelte.config.js
@@ -16,7 +16,10 @@ const config = {
 			fallback: undefined,
 			precompress: false,
 			strict: true
-	}
-)}};
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	}};
 
 export default config;
