4e155cae47a95679e1d77c0e6d70e89084f8858d
r20222 <101579892+r20222@users.noreply.github.com>
2023-12-08 15:39:34 +0100

    staticAdapter()
    werkt het nu op gh pages?

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		
-		adapter: adapter({
+		adapter: staticAdapter({
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
