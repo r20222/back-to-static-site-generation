37fd2072459819f2b196d2a6b0666dd157e04f52
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-12-15 10:42:08 +0100

    Basic static deployed sveltekit site

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import staticAdapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -8,16 +8,11 @@ const config = {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		
-		adapter: staticAdapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		}),
-		paths: {    base: '/back-to-static-site-generation',
+		adapter: adapter({
+			fallback: '404.html',
+		  }),
+		paths: {    
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH,
 		},
 	}};
 
