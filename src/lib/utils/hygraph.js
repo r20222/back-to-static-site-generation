471b67f99b8f43f23679020b7fe48ceded550e6c
CathelijneVisser <112855711+CathelijneVisser@users.noreply.github.com>
2023-12-15 14:04:01 +0100

    update .env

--- src/lib/utils/hygraph.js
+++ src/lib/utils/hygraph.js
@@ -1,9 +1,10 @@
-import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private'
 
 import { GraphQLClient } from 'graphql-request'
 
-export const hygraph = new GraphQLClient(HYGRAPH_URL, {
+export const hygraph = new GraphQLClient(GITHUB_HYGRAPH_URL, {
   headers: {
-    Authorization: `Bearer ${HYGRAPH_KEY}`,
+    Authorization: `Bearer ${GITHUB_HYGRAPH_KEY}`,
   },
 })
+
+
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,4 @@
 <script>
-	import { onMount } from "svelte";
 
 	// static site generation
 	export const prerender = true;
